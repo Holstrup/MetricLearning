b7aa989692fa2437257da4175476a2b4a46c3949
Cristian Botezatu <s164571@student.dtu.dk>
2019-04-12 12:25:26 +0200

    loss_function analysis and changes.

--- MetricLearning/LMNN.py
+++ MetricLearning/LMNN.py
@@ -57,33 +57,3 @@ def distance(xi, X):
     :return: Distance vector (1, N)
     """
     return np.linalg.norm((X - xi), axis=1)
-
-
-def find_triplets(xi, yi, X, y):
-    """
-    Given some vector xi and corresponding label yi, find target neighbors and imposters
-
-    :param xi: Embedding vector (1, F)
-    :param yi: Label for embedding vector
-    :param X: Full data matrix  (N, F)
-    :return: target_neighbors and imposters for embedding (K, F) (?, F)
-    """
-    candidate_target_neighbors = X[np.where(yi == y)]
-    imposters = X[np.where(yi != y)]
-
-    target_neighbors_dist = distance(xi, candidate_target_neighbors)
-    imposters_dist = distance(xi, imposters)
-
-    # Find K target neighbors
-    target_neighbors = np.zeros((1, F))
-    for i in range(K):
-        min_index = np.argmin(target_neighbors_dist)
-        target_neighbors = np.vstack((target_neighbors, candidate_target_neighbors[min_index]))
-        candidate_target_neighbors = np.delete(candidate_target_neighbors, (min_index), axis=0)
-    target_neighbors = target_neighbors[1:, :]
-
-    # Find ? imposters
-    max_target_dist = np.max(target_neighbors_dist)
-    imposters = imposters[np.where(imposters_dist < max_target_dist + l)]
-
-    return target_neighbors, imposters
